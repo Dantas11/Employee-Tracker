7a4c193231851883fc2799fea3f5d36155dea2a7
Thiago Dantas <thiagoo.dantas11@gmail.com>
2023-08-12 13:25:22 +1000

    others functions added to tracker.js

--- tracker.js
+++ tracker.js
@@ -91,3 +91,119 @@ showRoles = () => {
       InquirerPrompt();
   })
 };
+
+showEmployees = () => {
+  console.log('All employees are showing.');
+  const mysql = `SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary, CONCAT(mgr.first_name, mgr.last_name) AS manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee mgr ON employee.manager_id = mgr.id`;
+
+  connection.query(mysql, (err, rows) => {
+      if (err) return console.log(err);
+      console.table(rows);
+      InquirerPrompt();
+  });
+};
+
+addDepartments = () => {
+  inquirer.prompt([
+      {
+          type: 'input',
+          name: 'department',
+          message: 'What department do you want to add?',
+      }
+  ])
+      .then(answer => {
+          const mysql = `INSERT INTO department (name) VALUES (?)`;
+          connection.query(mysql, answer.department, (err, results) => {
+              if (err) return console.log(err);
+              console.log('Added' + answer.department + "to departments");
+
+              showDepartments();
+          });
+      });
+}
+
+addRoles = () => {
+  inquirer.prompt([
+      {
+          type: 'input',
+          name: 'roles',
+          message: "What do you want to add?",
+
+      },
+      {
+          type: 'input',
+          name: 'salary',
+          message: 'What is your yearly salary?',
+      }
+
+  ])
+      .then(answer => {
+          const parameters = [answer.roles, answer.salary];
+          const roles_var = `SELECT name, id FROM department`;
+
+          connection.query(roles_var, (err, data) => {
+              if (err) return console.log(err);
+              const department_var = data.map(({ name, id }) => ({ name: name, value: id }));
+
+              inquirer.prompt([
+                  {
+                      type: 'list',
+                      name: 'department_var',
+                      message: "What department is this role in?",
+                      choices: department_var
+                  }
+              ])
+                  .then(department_varChoice => {
+                      const department_var = department_varChoice.department_var;
+                      parameters.push(department_var);
+                      const mysql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`;
+
+                      connection.query(mysql, parameters, (err, result) => {
+                          if (err) return console.log(err);
+                          console.log('Added' + answer.roles + "to roles");
+                          showRoles();
+                      });
+                  });
+          });
+      });
+};
+
+addEmployees = ()  => {
+  inquirer.prompt([
+      {
+          type: 'input',
+          name: 'first_name',
+          message: 'Your First Name?',
+      },
+      {
+          type: 'input',
+          name: 'last_name',
+          message: 'Your Last Name?',
+      }
+  ])
+  .then(answer => {
+      const parameters = [answer.first_name, answer.last_name]
+      const roles_var = `SELECT roles.id, roles.title FROM roles`;
+
+      connection.query(roles_var, (err, data) => {
+          if(err) return console.log(err);
+          const roles = data.map(({ id, title }) => ({ name:title, value:id}));
+
+          inquirer.prompt([
+              {
+                  type: 'list',
+                  name: 'role',
+                  message: 'What is your role?',
+                  choices: roles
+              }
+          ])
+          .then(rolesChoice => {
+              const role = rolesChoice.roles;
+              parameters.push(roles);
+
+              showEmployees();
+
+         })
+      })
+  })
+}
