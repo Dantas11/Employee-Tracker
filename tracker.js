8b45e56b220255a5e91099d30678bd1f7ff841ad
Thiago Dantas <thiagoo.dantas11@gmail.com>
2023-08-12 13:16:34 +1000

    prompts info added to tracker.js

--- tracker.js
+++ tracker.js
@@ -8,3 +8,70 @@ const connection = mysql.createConnection({
     password: 'Dantas123',
     database: 'employee_db'
   });
+
+  connection.connect(function (err) {
+    if (err) return console.log(err);
+    InquirerPrompt();
+});
+
+const InquirerPrompt = () => {
+  inquirer.prompt([
+      {
+          type: 'list',
+          name: 'choices',
+          message: 'What would you like to do?',
+          choices: [
+              'View all departments',
+              'View all roles',
+              'View all employees',
+              'Add department',
+              'Add role',
+              'Add employee',
+              'Update all departments',
+              'Update employee infomation',
+              'Exit'
+          ]
+      }
+  ])
+
+      .then((answers) => {
+          const { choices } = answers;
+
+          if (choices === "View all departments") {
+              showDepartments();
+          }
+
+          if (choices === "View all roles") {
+              showRoles();
+          }
+
+          if (choices === "View all employees") {
+              showEmployees();
+          }
+
+          if (choices === "Add department") {
+              addDepartments();
+          }
+
+          if (choices === "Add role") {
+              addRoles();
+          }
+
+          if (choices === "Add employee") {
+              addEmployees();
+          }
+
+          if (choices === "Update all departments") {
+              allDepartments();
+          }
+
+          if (choices === "Update employee infomation") {
+              employeeInfomation();
+          }
+
+          if (choices === "Exit") {
+              connection.end();
+          }
+      });
+};
+
